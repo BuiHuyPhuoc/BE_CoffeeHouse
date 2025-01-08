b56564ed6c81004728d8348ab1c197d2f32e1e34
BuiHuyPhuoc <buihuyphuoc42@gmail.com>
2025-01-08 14:56:16 +0700

    init

---
+++ CoffeeHouseAPI/Helper/GENERATE_DATA.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+
+namespace CoffeeHouseAPI.Helper
+{
+    public static class GENERATE_DATA
+    {
+        public static string GenerateNumber(int stringLength)
+        {
+            Random random = new Random();
+            const string chars = "0123456789";
+            return new string(Enumerable.Repeat(chars, stringLength)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public static string GenerateString(int stringLength)
+        {
+            Random random = new Random();
+            const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, stringLength)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public static string GeneratePassword(int passwordLength) {
+            const string lowercase = "abcdefghijklmnopqrstuvwxyz";
+            const string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            const string specialChars = "!@#$%^&*()_-+=<>?.";
+
+            string allChars = lowercase + uppercase + specialChars;
+
+            StringBuilder result = new StringBuilder(passwordLength);
+            Random random = new Random();
+
+            result.Append(lowercase[random.Next(lowercase.Length)]);
+            result.Append(uppercase[random.Next(uppercase.Length)]);
+            result.Append(specialChars[random.Next(specialChars.Length)]);
+
+            for (int i = 3; i < passwordLength; i++)
+            {
+                result.Append(allChars[random.Next(allChars.Length)]);
+            }
+
+            return ShuffleString(result.ToString());
+        }
+
+        private static string ShuffleString(string str)
+        {
+            Random random = new Random();
+            char[] array = str.ToCharArray();
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                int j = random.Next(i + 1);
+                char temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+            return new string(array);
+        }
+    }
+}
---
+++ CoffeeHouseAPI/Helper/MappingProfiles.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using AutoMapper;
+using CoffeeHouseAPI.DTOs.Auth;
+using CoffeeHouseAPI.DTOs.Category;
+using CoffeeHouseLib.Models;
+
+namespace OrderService.Helper
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<CreateAccountRequest, Account>();
+            CreateMap<Account, CreateAccountRequest>();
+
+            CreateMap<Customer, CreateAccountRequest>();
+            CreateMap<CreateAccountRequest, Customer>();
+
+            CreateMap<RefreshTokenDTO, RefreshToken>();
+            CreateMap<RefreshToken, RefreshTokenDTO>();
+
+            CreateMap<Category, CategoryDTO>();
+            CreateMap<CategoryDTO, Category>();
+        }
+    }
+}
