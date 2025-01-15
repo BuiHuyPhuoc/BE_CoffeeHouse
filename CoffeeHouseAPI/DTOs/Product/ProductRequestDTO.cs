7bd302c3cd8250c9602a50c9461fa0167113fa60
nldangkhoa0712 <nldangkhoa0712@gmail.com>
2025-01-15 17:57:13 +0700

    [API] - Get Cateogory

--- CoffeeHouseAPI/DTOs/Product/ProductRequestDTO.cs
+++ CoffeeHouseAPI/DTOs/Product/ProductRequestDTO.cs
@@ -1,12 +1,14 @@
 ﻿using CoffeeHouseAPI.DTOs.Category;
 using CoffeeHouseAPI.DTOs.Image;
 using CoffeeHouseAPI.DTOs.ProductSize;
+using CoffeeHouseLib.Models;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Query.Expressions.Internal;
 
 namespace CoffeeHouseAPI.DTOs.Product
 {
     public class ProductModel
     {
+        public int? Id { get; set; } = null!;
         public string ProductName { get; set; } = null!;
 
         public string Description { get; set; } = null!;
@@ -14,17 +16,20 @@ namespace CoffeeHouseAPI.DTOs.Product
         public int CategoryId { get; set; }
 
         public bool IsValid { get; set; } = true;
+        public ImageResponseDTO ImageDefaultNavigation { get; set; } = null!;
+
     }
     public class ProductRequestDTO : ProductModel
     {
-        public List<ProductSizeRequestDTO> Sizes { get; set; } = new List<ProductSizeRequestDTO>();
+        public List<ProductSizeRequestDTO> ProductSizes { get; set; } = new List<ProductSizeRequestDTO>();
 
         public List<ImageRequestDTO> Images { get; set; } = new List<ImageRequestDTO>();
+       
     }
 
     public class ProductResponseDTO : ProductModel
     {
-        public List<ProductSizeRequestDTO> Sizes { get; set; } = new List<ProductSizeRequestDTO>();
+        public List<ProductSizeRequestDTO> ProductSizes { get; set; } = new List<ProductSizeRequestDTO>();
 
         public List<ImageResponseDTO> Images { get; set; } = new List<ImageResponseDTO>();
         public CategoryResponseDTO Category { get; set; } = null!;
