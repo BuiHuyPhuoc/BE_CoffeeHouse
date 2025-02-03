82230677bf4f10c5a0463a50238c8892016e31df
intern.tech01@itllogistics.vn <intern.tech01@itllogistics.vn>
2025-02-03 17:56:23 +0700

    [API] - Test API

---
+++ CoffeeHouseAPI/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeHouseAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Test")]
+        public IActionResult Test() {
+            return Ok();
+        }
+    }
+}
