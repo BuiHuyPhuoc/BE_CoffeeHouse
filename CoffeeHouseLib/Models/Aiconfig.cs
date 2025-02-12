4e623847e610b0d31246b5fe5510cde9f42df45d
intern.tech01@itllogistics.vn <intern.tech01@itllogistics.vn>
2025-02-12 20:26:30 +0700

    [API] - RecommendAI

---
+++ CoffeeHouseLib/Models/Aiconfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoffeeHouseLib.Models;
+
+public partial class Aiconfig
+{
+    public int Id { get; set; }
+
+    public string Promt { get; set; } = null!;
+
+    public string? Key { get; set; }
+}
